668067d175ce8428ec9fe7eb512f9c17d9f7c683
gyarmati.csanad <gyarmati.csanad@BURG-1E06-14>
2025-02-13 14:31:21 +0100

    horcsog.

--- JourneyPal/src/components/NavBar.tsx
+++ JourneyPal/src/components/NavBar.tsx
@@ -12,6 +12,24 @@ const NavBar: React.FC<NavBarProps> = ({ isDarkMode, toggleDarkMode }) => {
   return (
     <nav className="navbar">
       <h1 className="navbar-logo">JourneyPal</h1>
+<div aria-label="Orange and tan hamster running in a metal wheel" role="img" className="wheel-and-hamster">
+	<div className="wheel"></div>
+	<div className="hamster">
+		<div className="hamster__body">
+			<div className="hamster__head">
+				<div className="hamster__ear"></div>
+				<div className="hamster__eye"></div>
+				<div className="hamster__nose"></div>
+			</div>
+			<div className="hamster__limb hamster__limb--fr"></div>
+			<div className="hamster__limb hamster__limb--fl"></div>
+			<div className="hamster__limb hamster__limb--br"></div>
+			<div className="hamster__limb hamster__limb--bl"></div>
+			<div className="hamster__tail"></div>
+		</div>
+	</div>
+	<div className="spoke"></div>
+</div>
       <ul className="navbar-links">
         <li>
           <Link to="/">Home</Link>
