bbe22a0fb112c9dbbe65b9eeef83f4a04822d1d5
gyarmati.csanad <gyarmati.csanad@BURG-1E06-14>
2025-02-13 08:58:06 +0100

    signup es login teljes css es dark mode kompatibilitas

--- JourneyPal/src/components/SignUp.tsx
+++ JourneyPal/src/components/SignUp.tsx
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import '../JourneyPal.css';
+import '../SingUpLogin.css';
 
 const SignUp: React.FC = () => {
     const [formData, setFormData] = useState({
@@ -27,11 +28,11 @@ const SignUp: React.FC = () => {
         <div className="signup-container">
             <h2>Sign Up</h2>
             <form onSubmit={handleSubmit} className="signup-form">
-                <input type="text" name="username" placeholder="Username" value={formData.username} onChange={handleChange} required />
-                <input type="email" name="email" placeholder="Email" value={formData.email} onChange={handleChange} required />
-                <input type="password" name="password" placeholder="Password" value={formData.password} onChange={handleChange} required />
-                <input type="password" name="confirmPassword" placeholder="Confirm Password" value={formData.confirmPassword} onChange={handleChange} required />
-                <button type="submit">Sign Up</button>
+                <input type="text" name="username" placeholder="Username" value={formData.username} onChange={handleChange} required className='signup-input'/>
+                <input type="email" name="email" placeholder="Email" value={formData.email} onChange={handleChange} required className='signup-input' />
+                <input type="password" name="password" placeholder="Password" value={formData.password} onChange={handleChange} required className='signup-input'/>
+                <input type="password" name="confirmPassword" placeholder="Confirm Password" value={formData.confirmPassword} onChange={handleChange} required className='signup-input'/>
+                <button type="submit" className="signup-button">Sign Up</button>
             </form>
             <p>Already have an account? <Link to="/login">Log In</Link></p>
         </div>
